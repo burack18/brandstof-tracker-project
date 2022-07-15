99d6b9d36b0ac0610a715d91543484fb439f259a
burack <omerfaruk.aksak@student.ucll.be>
2022-07-15 09:49:11 +0200

    project init commit

---
+++ src/main/java/com/example/brandstoftracker/BrandstofTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.brandstoftracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandstofTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrandstofTrackerApplication.class, args);
+    }
+
+}
